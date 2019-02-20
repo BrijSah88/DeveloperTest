6d5c2368d450b6a4d7e66cf6b49484d1b3053aa8
BrijSah88 <47752566+BrijSah88@users.noreply.github.com>
2019-02-21 01:00:41 +0530

    Add files via upload

---
+++ FileInfo/BusinessLogic.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThirdPartyTools;
+using System.Configuration;
+
+namespace FileInfo
+{
+    public class BusinessLogic
+    {
+        /// <summary>
+        /// Reading input from user
+        /// </summary>                 
+        public void Read_Input_User()
+        {
+            try
+            {
+                Console.WriteLine("Please Enter Input (<functionality> <filename>)");
+                string strUserInput = Console.ReadLine();                
+                Console.WriteLine(Get_Version_Size(strUserInput));
+                Console.WriteLine();
+                Ask_Get_Input_Again();
+            }
+            catch (OutOfMemoryException ex)
+            {
+                Console.WriteLine("OutOfMemoryException from method CalculateVersionSize : " + ex.Message);
+                Ask_Get_Input_Again();
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine("ArgumentOutOfRangeException from method CalculateVersionSize : " + ex.Message);
+                Ask_Get_Input_Again();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception from method CalculateVersionSize : " + ex.Message);
+                Ask_Get_Input_Again();
+            }
+        }
+
+        /// <summary>
+        /// Ask to get user input again
+        /// </summary>
+        public void Ask_Get_Input_Again()
+        {
+            try
+            {
+                Console.WriteLine("Do you want Get Version Size again?{Y/N}");
+                string strYesNo = Console.ReadLine();
+                
+                if (strYesNo.ToUpper() == "Y")
+                    Read_Input_User();
+                else
+                    Environment.Exit(0);                
+            }
+            catch (OutOfMemoryException ex)
+            {
+                throw new Exception("OutOfMemoryException from method AsktoPerformAgain : " + ex.Message);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                throw new Exception("ArgumentOutOfRangeException from method AsktoPerformAgain : " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Exception from method AsktoPerformAgain : " + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Based on type of inputs function return version or size.
+        /// </summary>
+        public string Get_Version_Size(string strInputData)
+        {
+            try
+            {
+                //Applied Refactoring based on Failed and Pass cases
+                strInputData = strInputData.Trim();
+
+                string[] arrData = strInputData.Split(' ');
+                
+                if (arrData.Length == 2)
+                {
+                    string[] arrVersionList = ConfigurationManager.AppSettings["WhiteVersionList"].Trim().Split(',');
+                    string[] arrSizeList = ConfigurationManager.AppSettings["WhiteSizeList"].Trim().Split(',');
+
+                    FileDetails objFileDetails = new FileDetails();
+
+                    if (arrVersionList.Contains(arrData[0].Trim()))
+                    {
+                        return "File Version is : " + objFileDetails.Version(arrData[1].Trim());
+                    }
+                    else if (arrSizeList.Contains(arrData[0].Trim()))
+                    {
+                        return "File Size is : " + Convert.ToString(objFileDetails.Size(arrData[1].Trim()));
+                    }
+                    else
+                    {
+                        return "Invalid Option";
+                    }
+                }
+                else
+                {
+                    return "Invalid Input";
+                }                                                
+            }
+            catch (ArithmeticException ex)
+            {
+                return "ArithmeticException from method PerformAction : " + ex.Message;
+            }
+            catch (OutOfMemoryException ex)
+            {
+                return "OutOfMemoryException from method PerformAction : " + ex.Message;
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                return "IndexOutOfRangeException from method PerformAction : " + ex.Message;
+            }
+            catch (Exception ex)
+            {
+                return "Exception from method PerformAction: " + ex.Message;
+            }
+        }
+    }
+}
---
+++ FileInfo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ThirdPartyTools;
+using System.Configuration;
+
+namespace FileInfo
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Reading input from command line argument, if there is no argument then option is avilable to record from user side
+            BusinessLogic objResult = new BusinessLogic();
+            
+            //Reading from command line argument 
+            if (args.Length == 2)
+            {
+                Console.WriteLine(objResult.Get_Version_Size(args[0].Trim() + " " + args[1].Trim()));
+                Console.ReadLine();
+            }
+
+            //If command line argument not aviable then asking to read from user
+            else if (args.Length == 0)
+            {
+                Console.WriteLine("Input not found in command line argument, Do you want to give input from user side?{Y/N}");
+                string strYesNo = Console.ReadLine();
+                Console.WriteLine();
+                if (strYesNo.ToUpper() == "Y")
+                {
+                    objResult.Read_Input_User();
+                }                
+            }
+
+            //Command line argument is not in require format
+            else
+            {
+                Console.WriteLine("Invalid input passing from command line");
+                Console.ReadLine();
+            }
+        }       
+    }
+}
